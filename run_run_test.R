7765e4281cd8435e3dc3c9a3eb959c73d29587cb
Dan Thal <dthal@mathematica-mpr.com>
2020-05-11 11:35:01 -0400

    asd

--- run_run_test.R
+++ run_run_test.R
@@ -1,3 +1,3 @@
 args = commandArgs(trailingOnly=TRUE)
-rmarkdown::render('./run_test.rmd', params=list(grantee=args[1], group=args[2], outcome=args[3], warmup=as.numeric(args[4]), draws=as.numeric(args[5]), thin=as.numeric(args[6]), chains=as.numeric(args[7]), cores=as.numeric(args[7])))
-system(glue::glue('aws s3 cp run_test.html s3://50695-test/htmls/fake_{args[3]}_{args[1]}_{args[2]}.html'))
+rmarkdown::render('~/50695_docker_test/run_test.rmd', params=list(grantee=args[1], group=args[2], outcome=args[3], warmup=as.numeric(args[4]), draws=as.numeric(args[5]), thin=as.numeric(args[6]), chains=as.numeric(args[7]), cores=as.numeric(args[7])))
+system(glue::glue('aws s3 cp ~/50695_docker_test/run_test.html s3://50695-test/htmls/fake_{args[3]}_{args[1]}_{args[2]}.html'))
